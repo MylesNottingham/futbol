8e5bfa7dbddf18cad6ba0387166d9e9e7b6faa47
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-20 18:29:27 -0600

    Refactor: helper method for highest scoring home team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -96,14 +96,8 @@ class StatTracker
   end
 
   def highest_scoring_home_team
-    home_goals = Hash.new(0)
-    @game_teams.each do |game|
-      if game.home_or_away == "home"
-        home_goals[game.team_id] += game.goals.to_f
-      end
-    end
     home_pts_per_game = Hash.new(0)
-    home_goals.each do |team_id, goals|
+    total_goals_by_home_team.each do |team_id, goals|
       home_pts_per_game[team_id] = (goals / total_games_played_by_team[team_id])
     end
     highest_scoring_home = home_pts_per_game.max_by do |team_id, pts_per_game|
@@ -169,6 +163,16 @@ class StatTracker
     end
 
     total_goals_by_team
+  end
+
+  def total_goals_by_home_team
+    home_goals = Hash.new(0)
+    @game_teams.each do |game|
+      if game.home_or_away == "home"
+        home_goals[game.team_id] += game.goals.to_f
+      end
+    end
+    home_goals
   end
 
   def get_team_name(id)
