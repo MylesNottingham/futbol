b323d824b7d8cb135a00372960b24e8dc2f6a58e
Javen Wilson <123218127+javenb022@users.noreply.github.com>
2023-04-22 21:48:28 -0400

    Update stat_tracker_spec.rb
    I guess he needs more space? Hmm...fine

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -173,11 +173,13 @@ RSpec.describe StatTracker do
       expect(@stat_tracker.percentage(19881)).to eq(2.67)
     end
   end
+  
   describe "#number_of_games" do
     it "can get the total number of games as a float" do
       expect(@stat_tracker.number_of_games).to eq(7441.0)
     end
   end
+  
   describe "#total_games_played_by_team_in_games" do
     it "can find the total number of games played by a team" do
       expect(@stat_tracker.total_games_played_by_team_in_games).to be_a(Hash)
